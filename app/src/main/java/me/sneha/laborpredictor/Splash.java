aa1951f4511802b6141d18c26e35a92f85d1291e
harshadfriend <harshad.friend@gmail.com>
2018-05-15 12:52:18 +0530

    Initial commit

---
+++ app/src/main/java/me/sneha/laborpredictor/Splash.java
@@ -0,0 +1,42 @@
+package me.sneha.laborpredictor;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ProgressBar;
+
+public class Splash extends AppCompatActivity {
+    ProgressDialog pd;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        pd=new ProgressDialog(this);
+        pd.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        //pd.setMessage("Starting...");
+        //pd.show();
+
+        new CountDownTimer(2000,500){
+            @Override
+            public void onTick(long millisUntilFinished) {
+                Log.d("tick",""+millisUntilFinished/500);
+                if(((int)millisUntilFinished/1000)==0){
+                    pd.show();
+                }
+
+            }
+
+            @Override
+            public void onFinish() {
+                finish();
+                startActivity(new Intent(Splash.this,MainActivity.class));
+            }
+        }.start();
+    }
+}
