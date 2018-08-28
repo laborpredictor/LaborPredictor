e7dcf18e74b9a650be945a225daee669524760ea
harshadfriend <harshad.friend@gmail.com>
2018-08-28 15:35:58 +0530

    commit

--- app/src/main/java/me/sneha/laborpredictor/Predict.java
+++ app/src/main/java/me/sneha/laborpredictor/Predict.java
@@ -20,17 +20,19 @@ public class Predict extends AppCompatActivity {
     Spinner spFPType,spFPSubtype,spPPType,spPPSubtype;
     ArrayAdapter<String> adpType,adpFPNormalSubtype,adpFPAbnormalSubtype,adpPPNormalSubtype,adpPPAbnormalSubtype;
     String[] strType={"Normal","Abnormal"};
-    String[] strFPNormalSubtype={"Cephalic","Transverse"};
-    String[] strFPAbnormalSubtype={"Vertex","Occipito","Shoulder","Breech","Brow"};
+    String[] strFPNormalSubtype={"Cephalic","Vertex"};
+    String[] strFPAbnormalSubtype={"Transverse","Occipito","Shoulder","Breech","Brow"};
     String[] strPPNormalSubtype={"Posterior","Anterior"};
     String[] strPPAbnormalSubtype={"Low Lying","Placental Abruption","Placenta Bleeding","Placenta Pravia"};
     Button btnSubmit;
     double nfhrn=1,nfhra=0,nbpn=1,nbpa=0,ncln=1,ncla=0,ncephalic=0.875,nvertex=0.0416666667,ntransverse=0.0416666667,
             nbreech=0.0416666667,nposterior=0.3333333333,nanterior=0.6666666667,npbleeding=0,nppravia=0,
             nafyn=0.2916666667,nafya=0.7083333333;
-    double cfhrn=1,cfhra=0,cbpn=1,cbpa=0,ccln=1,ccla=0,ccephalic=0.875,cvertex=0.0416666667,ctransverse=0.0416666667,
-            cbreech=0.0416666667,cposterior=0.3333333333,canterior=0.6666666667,cpbleeding=0,cppravia=0,
-            cafyn=0.2916666667,cafya=0.7083333333;
+    double cfhrn=1,cfhra=0,cbpn=0.9375,cbpa=0.0625,ccln=0.875,ccla=0.125,ccephalic=0.375,cvertex=0.25,ctransverse=0.0625,
+            cbreech=0.3125,cposterior=0.4375,canterior=0.4375,cpbleeding=0.0625,cppravia=0.0625,
+            cafyn=0.4375,cafya=0.5625;
+    double evidenceN=0.6,evidenceC=0.4;
+    double normal=1,ceasar=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,7 +86,8 @@ public class Predict extends AppCompatActivity {
                 }
 
                 if(!etName.getText().toString().isEmpty() && !etBPSystol.getText().toString().isEmpty() && !etBPDiastol.getText().toString().isEmpty()
-                        && !etFHR.getText().toString().isEmpty() && !etCL.getText().toString().isEmpty() &&!etAF.getText().toString().isEmpty()){
+                        && !etFHR.getText().toString().isEmpty() && !etCL.getText().toString().isEmpty() &&!etAF.getText().toString().isEmpty())
+                {
 
 //                    dbhelper db=new dbhelper(Predict.this);
 //                    String result=db.enterdata(etName.getText().toString().trim(),etBPSystol.getText().toString().trim(),
@@ -95,6 +98,51 @@ public class Predict extends AppCompatActivity {
 //                        Toast.makeText(Predict.this, "Submitted !", Toast.LENGTH_SHORT).show();
 //                        onBackPressed();
 //                    }
+
+                    if(Integer.parseInt(etBPSystol.getText().toString())>125 || Integer.parseInt(etBPDiastol.getText().toString())>90){
+                        normal=normal*evidenceN*nbpn;
+                    }
+                    else{
+                        normal=normal*evidenceN*nbpa;
+                    }
+
+                    if(Integer.parseInt(etFHR.getText().toString())>165 && Integer.parseInt(etFHR.getText().toString())<105){
+                        normal=normal*nfhrn;
+                    }
+                    else{
+                        normal=normal*nfhra;
+                    }
+
+                    if(Double.parseDouble(etCL.getText().toString())>2.2 ){
+                        normal=normal*ncln;
+                    }
+                    else{
+                        normal=normal*ncla;
+                    }
+
+                    if(Integer.parseInt(etAF.getText().toString())>8 && Integer.parseInt(etAF.getText().toString())<5){
+                        normal=normal*nafyn;
+                    }
+                    else{
+                        normal=normal*nafya;
+                    }
+
+                    switch (spFPSubtype.getSelectedItem().toString().trim()){
+                        case "Cephalic":normal=normal*ncephalic;
+                            break;
+                        case "Vertex":normal=normal*nvertex;
+                            break;
+                        case "Transverse":normal=normal*ntransverse;
+                            break;
+                        case "Occipito":
+                            break;
+                        case "Shoulder":
+                            break;
+                        case "Breech":normal=normal*nbreech;
+                            break;
+                        case "Brow":
+                            break;
+                    }
 
                 }
 
